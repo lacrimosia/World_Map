32843c8424c3bcb6d4462b5b109596de2ab1da28
Shatilla Prayer <prayers@unlv.nevada.edu>
2015-09-30 10:50:18 -0700

    changed testing files, changed back to root

--- testing/js/controllers.js
+++ testing/js/controllers.js
@@ -28,74 +28,22 @@ angular.module('myApp.controllers', ['ui.bootstrap', 'ngAnimate'])
 
   })
 
-  .controller('HomeCtrl', function($scope, $http, $modal, Popup, Data, ModalBox, Position, GetMyLocation) {
+  .controller('HomeCtrl', function($scope, $http, Data, $modal) {
      
 
      Data.success(function(data) {
       $scope.data = data;
     });
 
-     $scope.oneAtATime = true;
-     $scope.change = true;
-     $scope.change1 = false;
-     $scope.theTitle = "";
-     $scope.theDescription = "";
-     $scope.clicked = false;
-     GetMyLocation.what();
-
-    $scope.toggleLightbox = function() {
-      $scope.data.caseStudyLightbox = !$scope.data.caseStudyLightbox;
-    }
-
-    $scope.openMe = function(){
-       ModalBox.OpenDiv(size, myFullImage, titleName, info);
-    };
-
-    $scope.ok = function () {
-        $scope.ModelInstance.close();
-         Popup.CloseMe('$scope.clicked');
-    };
-
-    $scope.prompt = function(index) {
-      $scope.data.things.forEach(function(thing) {
-        thing.promptOpen = true;
+    $scope.openMe = function(size, myFullImage, titleName, info){
+     $scope.ModelInstance = $modal.open({
+      animation: true,
+      template: '<div class="modalBox"><button class="btn btn-danger pull-right" type="button" ng-click="ok()" tooltip="Close"><i class="fa fa-times"></i></button><h1>'+titleName+'</h1><p>'+info+'<img src="img/'+myFullImage+'" class="img-responsive" alt="'+myFullImage+'" /></div>',
+      size: size,
+      scope: $scope
       });
-      var thing = $scope.data.things[index];
-      Popup.OpenMe('thing.promptOpen');
-    }
-
-    $scope.clearAll = function() {
-      $scope.data.things.forEach(function(thing) {
-        thing.promptOpen = false;
-      });
-    }
-
-    $scope.closePopup = function(thing) {
-      if (thing.choiceMade) {
-        Popup.CloseMe('thing.promptOpen');
-      }
-    }
-
-    $scope.popupStyle = function(thing) {
-      if (thing.choiceMade) {
-        return;
-      } else {
-        return {'top': Position.getY(thing.y) + '%', 'left': Position.getX(thing.x) + '%'};
-      }
-    }
-
-    $scope.pointStyle = function(thing) {
-      return {'top': Position.getY(thing.y) + '%', 'left': Position.getX(thing.x) + '%'};
-    }
-
-    $scope.makeSelection = function(thing, selection) {
-      thing.selection = selection;
-      thing.choiceMade = true;
-      $scope.clicked = true;
-    }
-
-    $scope.unCheck = function(){
-       Popup.CloseMe('$scope.clicked');
     };
+
+  
 
   });
