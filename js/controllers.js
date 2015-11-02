fdefcb93896de4457cf0151b34dd9a597c634292
Shatilla Prayer <prayers@unlv.nevada.edu>
2015-11-02 14:46:04 -0800

    format changes

--- js/controllers.js
+++ js/controllers.js
@@ -1,34 +1,32 @@
-
-
 angular.module('myApp.controllers', ['ui.bootstrap'])
 
-  .controller('AppCtrl', function($scope, $http, Data) {
+.controller('AppCtrl', function($scope, $http, Data) {
 
     $scope.includes = {
-      // oeMenu: 'partials/oe-menu.html',
-      // oeHeader: 'partials/oe-header.html',
-      oeFooter: 'partials/oe-footer.html'
+        // oeMenu: 'partials/oe-menu.html',
+        // oeHeader: 'partials/oe-header.html',
+        oeFooter: 'partials/oe-footer.html'
     }
 
     $scope.oeMenu = {
-      open: false
+        open: false
     }
 
     $scope.oeMenuToggle = function(toggle) {
-      if(toggle == 'off') {
-        $scope.oeMenu.open = false;
-      } else {
-        $scope.oeMenu.open = !$scope.oeMenu.open;
-      }
+        if (toggle == 'off') {
+            $scope.oeMenu.open = false;
+        } else {
+            $scope.oeMenu.open = !$scope.oeMenu.open;
+        }
     }
 
     Data.success(function(data) {
-      $scope.data = data;
+        $scope.data = data;
     });
 
-  })
+})
 
-  .controller('HomeCtrl', [
+.controller('HomeCtrl', [
     '$scope',
     '$http',
     '$uibModal',
@@ -36,84 +34,91 @@ angular.module('myApp.controllers', ['ui.bootstrap'])
     'Data',
     function($scope, $http, $uibModal, modalService, Data) {
 
-     $scope.oneAtATime = true;
-     $scope.change = true;
-     $scope.theTitle = "";
-     $scope.theDescription = "";
-     $scope.clicked = false;
-     $scope.close = false;
-     $scope.openBook = false;
-     $scope.slide = false;
+        $scope.oneAtATime = true;
+        $scope.change = true;
+        $scope.theTitle = "";
+        $scope.theDescription = "";
+        $scope.clicked = false;
+        $scope.close = false;
+        $scope.openBook = false;
+        $scope.slide = false;
 
 
-    $scope.toggleLightbox = function() {
-      $scope.data.caseStudyLightbox = !$scope.data.caseStudyLightbox;
+        $scope.toggleLightbox = function() {
+            $scope.data.caseStudyLightbox = !$scope.data.caseStudyLightbox;
+        }
+
+
+        // Modal calls 
+
+        $scope.openMe = function(title, description, myFullImage) {
+            modalService.openMenuModal('partials/FullSizeModal.html', title, description, myFullImage);
+        };
+
+        $scope.openHelpMenu = function(size, title, description) {
+            modalService.openMenuModal('partials/help.html');
+        };
+
+        $scope.openBook = function(templateLink, title, description, myFullImage, Link, theImage, index, array) {
+            modalService.openMenuModal('partials/modal.html', title, description, myFullImage, Link, theImage, index, array);
+        };
+
+        $scope.openUNLV = function(templateLink, title, description, myFullImage, Link, theImage, index, array) {
+            modalService.openMenuModal('partials/unlv.html', title, description, myFullImage, Link, theImage, index, array);
+        };
+
+        $scope.prompt = function(index) {
+            $scope.data.things.forEach(function(thing) {
+                thing.promptOpen = true;
+            });
+            var thing = $scope.data.things[index];
+
+            // alert(thing.Books[index].name);
+            thing.promptOpen = true;
+        }
+
+        $scope.clearAll = function() {
+            $scope.data.things.forEach(function(thing) {
+                thing.promptOpen = false;
+            });
+        }
+
+        $scope.closePopup = function(thing) {
+            if (thing.choiceMade) {
+                thing.promptOpen = false;
+            }
+        }
+
+        $scope.popupStyle = function(thing) {
+            if (thing.choiceMade) {
+                return;
+            } else {
+                return {
+                    'top': thing.y + '%',
+                    'left': thing.x + '%'
+                };
+            }
+        }
+
+        $scope.pointStyle = function(thing) {
+            return {
+                'top': thing.y + '%',
+                'left': thing.x + '%'
+            };
+        }
+
+        $scope.makeSelection = function(thing, selection) {
+            thing.selection = selection;
+            thing.choiceMade = true;
+            $scope.clicked = true;
+            console.log($scope.clicked);
+        }
+
+        $scope.unCheck = function() {
+            $scope.clicked = false;
+
+            // console.log($scope.clicked);
+        };
+
     }
-
-
-    // Modal calls 
-
-    $scope.openMe = function(title, description, myFullImage){
-      modalService.openMenuModal('partials/FullSizeModal.html', title, description, myFullImage);
-    };
-
-    $scope.openHelpMenu = function(size, title, description){
-      modalService.openMenuModal('partials/help.html');
-    };
-
-    $scope.openBook = function(templateLink, title, description, myFullImage, Link, theImage, index, array){
-      modalService.openMenuModal('partials/modal.html', title, description, myFullImage, Link, theImage, index, array);
-    };
-
-    $scope.openUNLV = function(templateLink, title, description, myFullImage, Link, theImage, index, array){
-      modalService.openMenuModal('partials/unlv.html', title, description, myFullImage, Link, theImage, index, array);
-    };
-
-    $scope.prompt = function(index) {
-      $scope.data.things.forEach(function(thing) {
-        thing.promptOpen = true;
-      });
-      var thing = $scope.data.things[index];
-
-      // alert(thing.Books[index].name);
-      thing.promptOpen = true;
-    }
-
-    $scope.clearAll = function() {
-      $scope.data.things.forEach(function(thing) {
-        thing.promptOpen = false;
-      });
-    }
-
-    $scope.closePopup = function(thing) {
-      if (thing.choiceMade) {
-        thing.promptOpen = false;
-      }
-    }
-
-    $scope.popupStyle = function(thing) {
-      if (thing.choiceMade) {
-        return;
-      } else {
-        return {'top': thing.y + '%', 'left': thing.x + '%'};
-      }
-    }
-
-    $scope.pointStyle = function(thing) {
-      return {'top': thing.y + '%', 'left': thing.x + '%'};
-    }
-
-    $scope.makeSelection = function(thing, selection) {
-      thing.selection = selection;
-      thing.choiceMade = true;
-      $scope.clicked = true;
-       console.log($scope.clicked);
-    }
-
-    $scope.unCheck = function(){
-       $scope.clicked=false;
-
-       // console.log($scope.clicked);
-    };
-
-  }]);
+]);
